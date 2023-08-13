957fe3589eb9d1b6152637d0f8393264828109d7
Matheus Bastos <matheus.mab432@gmail.com>
2023-08-13 01:08:48 -0300

    add primitive variables

--- code/01-fundamentals/fundamentals/fundamentals.cpp
+++ code/01-fundamentals/fundamentals/fundamentals.cpp
@@ -34,5 +34,40 @@ int main() {
   cout << "The area of the room is " << room_width * room_length << "m"
        << endl;
 
+  // ? Character types
+  char middle_initial{ 'J' };
+
+  // ? Integer types
+  unsigned short int exam_score{ 55 };
+
+  int countries_represented{ 65 };
+
+  long people_in_florida{ 20610000 };
+  // * The single quotes can make it easier to read large numbers (C++14)
+  // NOTE The list initialization will error if this overflows (if the variable was a long instead of a long long)
+  long long people_on_earth{ 7'600'000'000 };
+
+  // ? Floating point types
+  float car_payment{ 401.23 };
+
+  double pi{ 3.14159 };
+
+  // * (2.7 * 10^120)
+  long double large_amount{ 2.7e120 };
+
+  // ? Boolean types
+  bool game_over{ false };
+  // NOTE bools will print as 0 or 1
+  cout << "The value of gameOver is " << game_over << endl;
+
+  // ? Overflow example
+  short value1{ 30000 };
+  short value2{ 1000 };
+  short product{ value1 * value2 };
+
+  // NOTE will overflow into a negative number in this instance
+  cout << "The product of " << value1 << " and " << value2 << " is " << product
+       << endl;
+
   // Variables
 }
---
+++ notes/02-types.md
@@ -0,0 +1,11 @@
+# C++ Types 🎮
+
+## Primitive Types
+
+_Primitive types_ are the most basic types in C++. They are the building blocks for all other types. Some of them are:
+
+- _character types_ - are used to represent characters (e.g. `char`, `wchar_t`).
+- _numerical integer types_ - are used to represent integers (e.g. `int`, `long`, `short`, `long long`) and their unsigned counterparts (e.g. `unsigned int`, `unsigned long`, ...).
+- _floating-point types_ - are used to represent real numbers (e.g. `float`, `double`, `long double`).
+
+- **The size and precision of the types is often compiler-dependent**. The `sizeof()` operator can be used to get the size of a type in bytes.
