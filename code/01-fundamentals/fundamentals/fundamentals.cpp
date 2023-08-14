a3ac7f255941611bb6a47829c9b88d9bd9428253
Matheus Almeida Bastos <matheus.mab432@gmail.com>
2023-08-14 08:54:30 -0300

    add sizeof operator

--- code/01-fundamentals/fundamentals/fundamentals.cpp
+++ code/01-fundamentals/fundamentals/fundamentals.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+// ? Including climits to get `sizeof` for integer types
+#include <climits>
 
 using std::cin;
 using std::cout;
@@ -10,7 +12,7 @@ int global_var{ 10 };
 // TODO move each section into its own file
 int main() {
   // Variables
-
+  /*
   cout << "Global var: " << global_var << endl;
 
   // ? Different ways to initialize variables
@@ -44,8 +46,9 @@ int main() {
 
   long people_in_florida{ 20610000 };
   // * The single quotes can make it easier to read large numbers (C++14)
-  // NOTE The list initialization will error if this overflows (if the variable was a long instead of a long long)
-  long long people_on_earth{ 7'600'000'000 };
+  // NOTE The list initialization will error if this overflows (if the variable
+  // was a long instead of a long long)
+  long long people_on_earth { 7'600'000'000 };
 
   // ? Floating point types
   float car_payment{ 401.23 };
@@ -68,6 +71,54 @@ int main() {
   // NOTE will overflow into a negative number in this instance
   cout << "The product of " << value1 << " and " << value2 << " is " << product
        << endl;
+  */
+
+  // ? sizeof operator - depends on specific c++ version and compiler
+  cout << "char: " << sizeof(char) << " bytes" << endl;
+  cout << "int: " << sizeof(int) << " bytes" << endl;
+  cout << "unsigned int: " << sizeof(unsigned int) << " bytes" << endl;
+  cout << "short: " << sizeof(short) << " bytes" << endl;
+  cout << "long: " << sizeof(long) << " bytes" << endl;
+  cout << "long long: " << sizeof(long long) << " bytes" << endl;
+
+  cout << "========================" << endl;
+
+  cout << "float: " << sizeof(float) << " bytes" << endl;
+  cout << "double: " << sizeof(double) << " bytes" << endl;
+  cout << "long double: " << sizeof(long double) << " bytes" << endl;
+
+  cout << "========================" << endl;
+
+  cout << "Minimum values:" << endl;
+
+  cout << "char: " << CHAR_MIN << endl;
+  cout << "int: " << INT_MIN << endl;
+  cout << "short: " << SHRT_MIN << endl;
+  cout << "long: " << LONG_MIN << endl;
+  cout << "long long: " << LLONG_MIN << endl;
+
+  cout << "========================" << endl;
+
+  cout << "Maximum values:" << endl;
+
+  cout << "char: " << CHAR_MAX << endl;
+  cout << "int: " << INT_MAX << endl;
+  cout << "short: " << SHRT_MAX << endl;
+  cout << "long: " << LONG_MAX << endl;
+  cout << "long long: " << LLONG_MAX << endl;
+
+  cout << "========================" << endl;
+
+  cout << "sizeof using variable names" << endl;
+
+  int age{ 21 };
+  cout << "age is " << sizeof(age) << " bytes" << endl;
+  // NOTE sizeof is an operator, not a function, so the parentheses are optional
+  cout << "age is " << sizeof age << " bytes" << endl;
+
+  double wage{ 22.24 };
+  cout << "wage is " << sizeof(wage) << " bytes" << endl;
+  cout << "wage is " << sizeof wage << " bytes" << endl;
 
   // Variables
 }
