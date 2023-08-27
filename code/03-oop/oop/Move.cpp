af52e6579008a73cd97744dcb44738654ae7dfa7
Matheus Bastos <matheus.mab432@gmail.com>
2023-08-27 14:25:21 -0300

    add move semantics

---
+++ code/03-oop/oop/Move.cpp
@@ -0,0 +1,9 @@
+﻿#include "Move.h"
+
+#include <iostream>
+using namespace std;
+
+Move::Move(Move&& source) : data(source.data) {
+    // NOTE It's important to null the source pointer, as it's now a dangling pointer
+    source.data = nullptr;
+}
---
+++ code/03-oop/oop/Move.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+class Move {
+private:
+    int* data;
+
+public:
+    void set_data_value(int d) {
+        *data = d;
+    }
+
+    int get_data_value() {
+        return *data;
+    }
+
+    Move(int d);
+    Move(const Move& source);
+    // NOTE Move constructor
+    Move(Move&& source);
+    ~Move();
+};
